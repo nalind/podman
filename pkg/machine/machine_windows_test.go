ce8df2391443a375e7530033de912b43f08aa834
Xiaowen-Yang <xiaowenyang52@gmail.com>
2026-09-13 11:53:40 +0200

    machine: clean up orphaned gvproxy and win-sshproxy when starting Windows machines
    Fixes: #29474
    Signed-off-by: Xiaowen-Yang <xiaowenyang52@gmail.com>

--- pkg/machine/machine_windows_test.go
+++ pkg/machine/machine_windows_test.go
@@ -3,12 +3,48 @@
 package machine
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
+	winio "github.com/Microsoft/go-winio"
 	"github.com/stretchr/testify/require"
+	"go.podman.io/podman/v6/pkg/machine/define"
 )
+
+func testNamedPipe(t *testing.T) (string, func() error) {
+	t.Helper()
+
+	pipeName := fmt.Sprintf("podman-machine-test-%d", os.Getpid())
+	listener, err := winio.ListenPipe(`\\.\pipe\`+pipeName, nil)
+	require.NoError(t, err)
+	t.Cleanup(func() { _ = listener.Close() })
+	return pipeName, listener.Close
+}
+
+// A stale proxy is cleaned up when its state file has already supplied the PID.
+func TestCleanupStaleProxy(t *testing.T) {
+	pipeName, closePipe := testNamedPipe(t)
+	cleaned := false
+	err := cleanupStaleProxy(pipeName, uint32(os.Getpid()), func() error {
+		cleaned = true
+		return closePipe()
+	})
+
+	require.NoError(t, err)
+	require.True(t, cleaned)
+}
+
+func TestCleanupStaleGVProxyFailsWithoutPIDFile(t *testing.T) {
+	pipeName, closePipe := testNamedPipe(t)
+	defer func() { _ = closePipe() }()
+
+	pidFile := define.VMFile{Path: filepath.Join(t.TempDir(), "missing.pid")}
+	err := CleanupStaleGVProxy(pipeName, pidFile)
+	require.ErrorContains(t, err, "reading gvproxy PID file")
+}
 
 // CreateNewItemWithPowerShell creates a new item using PowerShell.
 // It's an helper to easily create junctions on Windows (as well as other file types).
